c6fbedf96e4d1b21e9ceae2a94d09f0e8ae15794
jgrisalesquintero <juan.grisalesquintero@mercadolibre.com.co>
2024-02-17 11:58:03 -0500

    Creacion de estructura del proyecto con las entidades mapeadas. Se debe revisar la implementacion de las listas en buyer y seller o si hacerlas con un map en los repositorios

---
+++ src/main/java/com/example/be_java_hisp_w25_g11/entity/Buyer.java
@@ -0,0 +1,13 @@
+package com.example.be_java_hisp_w25_g11.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Buyer {
+    private Long id;
+    private String name;
+}
