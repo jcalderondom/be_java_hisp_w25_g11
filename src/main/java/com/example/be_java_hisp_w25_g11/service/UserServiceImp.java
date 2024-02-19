7061dd6ccbfbe4a42d3b89b0c268a42302227f15
David Mateo García Vallejo <davidmgarciav@gmail.com>
2024-02-19 16:08:23 -0500

    US01
    First version of Follow method and private method to search a user in both repositories, buyer and seller repositories.

--- src/main/java/com/example/be_java_hisp_w25_g11/service/UserServiceImp.java
+++ src/main/java/com/example/be_java_hisp_w25_g11/service/UserServiceImp.java
@@ -4,20 +4,58 @@ import com.example.be_java_hisp_w25_g11.dto.response.FollowedDTO;
 import com.example.be_java_hisp_w25_g11.dto.response.FollowersCountDTO;
 import com.example.be_java_hisp_w25_g11.dto.response.FollowersDTO;
 import com.example.be_java_hisp_w25_g11.dto.SuccessDTO;
+import com.example.be_java_hisp_w25_g11.entity.Buyer;
+import com.example.be_java_hisp_w25_g11.entity.Seller;
 import com.example.be_java_hisp_w25_g11.repository.buyer.IBuyerRepository;
+import com.example.be_java_hisp_w25_g11.repository.seller.ISellerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImp implements IUserService {
     private IBuyerRepository buyerRepository;
+    private ISellerRepository sellerRepository;
 
-    public UserServiceImp(IBuyerRepository buyerRepository) {
+    public UserServiceImp(
+        IBuyerRepository buyerRepository,
+        ISellerRepository sellerRepository
+    ) {
         this.buyerRepository = buyerRepository;
+        this.sellerRepository = sellerRepository;
+    }
+
+    private Object getUser(Long id) {
+        if (buyerRepository.existing(id)) {
+            return buyerRepository.get(id).get();
+        } else if (sellerRepository.existing(id)) {
+            return sellerRepository.get(id).get();
+        } else {
+            throw new NotFoundException("El usuario con id="+id+" no existe."); // TODO NotFoundException
+        }
     }
 
     @Override
-    public SuccessDTO follow(Long userIdToFollow) {
-        return null;
+    public SuccessDTO follow(Long userId, Long userIdToFollow) {
+        Object user = getUser(userId);
+        Object userToFollow = getUser(userIdToFollow);
+
+        if (!(userToFollow instanceof Seller)) {
+            throw new BadRequestException("El comprador a seguir debe ser un vendedor."); // TODO BadRequestException
+        }
+
+        if (user instanceof Buyer) {
+            if (!((Buyer) user).getFollowed().contains(userIdToFollow)) {
+                throw new BadRequestException("El comprador con id="+userId+" ya sigue al vendedor."); // TODO BadRequestException
+            }
+            ((Buyer) user).getFollowed().add(userIdToFollow);
+            ((Seller) userToFollow).getFollowers().add(userId);
+        } else if (user instanceof Seller) {
+            if (!((Seller) user).getFollowed().contains(userIdToFollow)) {
+                throw new BadRequestException("El vendedor con id="+userId+" ya sigue al vendedor."); // TODO BadRequestException
+            }
+            ((Seller) user).getFollowed().add(userIdToFollow);
+            ((Seller) userToFollow).getFollowers().add(userId);
+        }
+        return new SuccessDTO("El usuario con id="+userId+" ahora sigue al usuario con id="+userIdToFollow+".");
     }
 
     @Override
