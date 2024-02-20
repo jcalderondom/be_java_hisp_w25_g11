09ea8a9cae394333a11bed21d8de893aaf824691
Danis Abadia <danis.abadia@mercadolibre.com.co>
2024-02-19 20:20:29 -0500

    CHORE: Merge remote branch with local branch

--- src/main/java/com/example/be_java_hisp_w25_g11/service/IUserService.java
+++ src/main/java/com/example/be_java_hisp_w25_g11/service/IUserService.java
@@ -7,8 +7,8 @@ import com.example.be_java_hisp_w25_g11.dto.SuccessDTO;
 
 public interface IUserService {
     SuccessDTO follow(Long userId, Long userIdToFollow);
-    FollowersCountDTO followersSellersCount(Long sellerId);
-    FollowersDTO buyersFollowSellers(Long sellerId);
+    FollowerCountDTO followersSellersCount(Long sellerId);
+    FollowerDTO buyersFollowSellers(Long sellerId);
     FollowedDTO sellersFollowingByUsers(Long userId);
     SuccessDTO unfollow(Long userId, Long sellerIdToUnfollow);
     FollowerDTO sortFollowers(String order);
--- src/main/java/com/example/be_java_hisp_w25_g11/service/UserServiceImp.java
+++ src/main/java/com/example/be_java_hisp_w25_g11/service/UserServiceImp.java
@@ -7,9 +7,8 @@ import com.example.be_java_hisp_w25_g11.dto.response.FollowerDTO;
 import com.example.be_java_hisp_w25_g11.dto.SuccessDTO;
 import com.example.be_java_hisp_w25_g11.entity.Buyer;
 import com.example.be_java_hisp_w25_g11.entity.Seller;
+import com.example.be_java_hisp_w25_g11.exception.BadRequestException;
 import com.example.be_java_hisp_w25_g11.exception.NotFoundException;
-import com.example.be_java_hisp_w25_g11.repository.buyer.BuyerRepositoryImp;
-import com.example.be_java_hisp_w25_g11.repository.seller.SellerRepositoryImp;
 import org.modelmapper.ModelMapper;
 import com.example.be_java_hisp_w25_g11.repository.buyer.IBuyerRepository;
 import com.example.be_java_hisp_w25_g11.repository.seller.ISellerRepository;
@@ -19,14 +18,14 @@ import java.util.Optional;
 
 @Service
 public class UserServiceImp implements IUserService {
-    private IBuyerRepository buyerRepository;
-    private ISellerRepository sellerRepository;
-    private ModelMapper mapper;
+    private final IBuyerRepository buyerRepository;
+    private final ISellerRepository sellerRepository;
+    private final ModelMapper mapper;
 
     public UserServiceImp(
         IBuyerRepository buyerRepository,
         ISellerRepository sellerRepository,
-        ModelMapper mapper,
+        ModelMapper mapper
     ) {
         this.buyerRepository = buyerRepository;
         this.sellerRepository = sellerRepository;
@@ -39,7 +38,7 @@ public class UserServiceImp implements IUserService {
         } else if (sellerRepository.existing(id)) {
             return sellerRepository.get(id).get();
         } else {
-            throw new NotFoundException("El usuario con id="+id+" no existe."); // TODO NotFoundException
+            throw new NotFoundException("El usuario con id="+id+" no existe.");
         }
     }
 
@@ -49,23 +48,23 @@ public class UserServiceImp implements IUserService {
         Object userToFollow = getUser(userIdToFollow);
 
         if (!(userToFollow instanceof Seller)) {
-            throw new BadRequestException("El comprador a seguir debe ser un vendedor."); // TODO BadRequestException
+            throw new BadRequestException("El comprador a seguir debe ser un vendedor.");
         }
 
         if (user instanceof Buyer) {
             if (((Buyer) user).getFollowed().contains(userIdToFollow)) {
-                throw new BadRequestException("El comprador con id="+userId+" ya sigue al vendedor con id"+userToFollow+"."); // TODO BadRequestException
+                throw new BadRequestException("El comprador con id="+userId+" ya sigue al vendedor con id"+userToFollow+".");
             }
             ((Buyer) user).getFollowed().add(userIdToFollow);
             ((Seller) userToFollow).getFollowers().add(userId);
         } else if (user instanceof Seller) {
             if (((Seller) user).getFollowed().contains(userIdToFollow)) {
-                throw new BadRequestException("El vendedor con id="+userId+" ya sigue al vendedor con id"+userToFollow+"."); // TODO BadRequestException
+                throw new BadRequestException("El vendedor con id="+userId+" ya sigue al vendedor con id"+userToFollow+".");
             }
             ((Seller) user).getFollowed().add(userIdToFollow);
             ((Seller) userToFollow).getFollowers().add(userId);
         } else {
-            throw new BadRequestException("El usuario con id="+userId+" no es ni comprador ni vendedor."); // TODO BadRequestException
+            throw new BadRequestException("El usuario con id="+userId+" no es ni comprador ni vendedor.");
         }
         return new SuccessDTO("El usuario con id="+userId+" ahora sigue al vendedor con id="+userIdToFollow+".");
     }
@@ -86,7 +85,7 @@ public class UserServiceImp implements IUserService {
 
     @Override
     public FollowerDTO buyersFollowSellers(Long sellerId) {
-        Optional<Seller> seller = sellerRepositoryImp.get(sellerId);
+        Optional<Seller> seller = sellerRepository.get(sellerId);
         if(seller.isEmpty()){
             throw new NotFoundException("Buyer does not exists");
         }
@@ -102,7 +101,7 @@ public class UserServiceImp implements IUserService {
                     }
                     else{
 
-                        return sellerRepositoryImp.get(followerId);
+                        return sellerRepository.get(followerId);
                     }
                 })
                 .map(u -> {return mapper.map(u.get(), UserDTO.class);})
@@ -122,18 +121,18 @@ public class UserServiceImp implements IUserService {
 
         if (user instanceof Buyer) {
             if (!((Buyer) user).getFollowed().contains(sellerIdToUnfollow)) {
-                throw new BadRequestException("El comprador con id="+userId+" no sigue al vendedor con id"+userToUnfollow+"."); // TODO BadRequestException
+                throw new BadRequestException("El comprador con id="+userId+" no sigue al vendedor con id"+userToUnfollow+".");
             }
             ((Buyer) user).getFollowed().remove(sellerIdToUnfollow);
             ((Seller) userToUnfollow).getFollowers().remove(userId);
         } else if (user instanceof Seller) {
             if (!((Seller) user).getFollowed().contains(sellerIdToUnfollow)) {
-                throw new BadRequestException("El vendedor con id="+userId+" no sigue al vendedor con id"+userToUnfollow+"."); // TODO BadRequestException
+                throw new BadRequestException("El vendedor con id="+userId+" no sigue al vendedor con id"+userToUnfollow+".");
             }
             ((Seller) user).getFollowed().remove(sellerIdToUnfollow);
             ((Seller) userToUnfollow).getFollowers().remove(userId);
         } else {
-            throw new BadRequestException("El usuario con id="+userId+" no es ni comprador ni vendedor."); // TODO BadRequestException
+            throw new BadRequestException("El usuario con id="+userId+" no es ni comprador ni vendedor.");
         }
         return new SuccessDTO("El usuario con id="+userId+" ha dejado de seguir al vendedor con id="+sellerIdToUnfollow+".");
     }
