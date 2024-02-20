11cb5d77a23a4146a9610647d08f938ddc9b9a8f
Danis Abadia <danis.abadia@mercadolibre.com.co>
2024-02-19 21:07:21 -0500

    FIX: Bad Merge in GlobalExceptionHandler

--- src/main/java/com/example/be_java_hisp_w25_g11/exception/NotFoundExeption.java
+++
@@ -1,7 +0,0 @@
-package com.example.be_java_hisp_w25_g11.exception;
-
-public class NotFoundExeption extends RuntimeException{
-    public NotFoundExeption(String message){
-        super(message);
-    }
-}
