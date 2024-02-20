bfa0fce79e89ed3325c337d153c7d5c1a77c41c5
Danis Abadia <danis.abadia@mercadolibre.com.co>
2024-02-19 21:07:31 -0500

    FIX: Bad Merge in GlobalExceptionHandler

--- src/main/java/com/example/be_java_hisp_w25_g11/service/IUserService.java
+++ src/main/java/com/example/be_java_hisp_w25_g11/service/IUserService.java
@@ -6,12 +6,13 @@ import com.example.be_java_hisp_w25_g11.dto.response.FollowerDTO;
 import com.example.be_java_hisp_w25_g11.dto.SuccessDTO;
 
 public interface IUserService {
+
     SuccessDTO follow(Long userId, Long userIdToFollow);
     FollowerCountDTO followersSellersCount(Long sellerId);
     FollowerDTO buyersFollowSellers(Long sellerId);
     FollowedDTO sellersFollowingByUsers(Long userId);
     SuccessDTO unfollow(Long userId, Long sellerIdToUnfollow);
-    FollowerDTO sortFollowers(String order);
-    FollowedDTO sortFollowed(String order);
+    FollowerDTO sortFollowers(Long id, String order);
+    FollowedDTO sortFollowed(Long id,String order);
     boolean isSeller(Long userId);
 }
